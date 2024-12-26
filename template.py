55ce3c62c6eb82a1d37ae3fb8ef7fdc3e1cacbe1
riyosebB <riyosebastian2001@gmail.com>
2024-12-26 14:00:59 -0500

    Project Floder Structures

---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level= logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+project_name = "Text_Summarizer"
+
+
+list_of_files = [
+    ".github/workflows/.getkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/logging/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config/yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb  "
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath) #Using the Path function to identify and associate each path according to the system os
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok= True)
+        logging.info(f"Creating Directory: {filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    
+    else:
+        logging.info(f'{filename} already exists.')
